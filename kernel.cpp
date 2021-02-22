045def207461a5238906ba5f74207beeb6ced7cf
luliyu <coordinate@live.com>
2021-02-22 22:28:40 +0800

    add types.h

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,7 @@
+#include "types.h"
+
 void printf(char* str) {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
     for (int i = 0; str[i]; i++) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     }
@@ -15,7 +17,7 @@ extern "C" void callConstructors() {
     }
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     printf((char*)"hello world!");
     while(1);
 }
